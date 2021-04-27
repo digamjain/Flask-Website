3319af01883c2c642f9443adfbfa3714d0357e06
Digam <digamj@gmail.com>
2021-04-28 05:12:58 +0530

    Added new route in market.py Templates   ->Added market.html with purchase tables

--- market.py
+++ market.py
@@ -8,3 +8,13 @@ app = Flask(__name__)
 @app.route('/home')
 def homepage():
     return render_template('home.html')
+
+#Send data to templates
+@app.route('/market')
+def market_page():
+    items = [
+    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', item = items)
