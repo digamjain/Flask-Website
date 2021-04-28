ec2f3c7166d364c05ca5cee1467f0fe05c6b81eb
Digam <digamj@gmail.com>
2021-04-29 04:13:16 +0530

    Version 0.7 Added validators in the form fields requirements.txt   ->Added a new requirement market   ->forms.py     ->Added validators in the fields   ->routes.py     ->Added if loop to submit changes if validators satisfy the condition     ->Added conditional loop to display error on the server side if any     ->templates       ->register.html         ->Added a hidden field token         ->Minute changes to the text fields

--- market/forms.py
+++ market/forms.py
@@ -2,10 +2,12 @@
 from flask_wtf import FlaskForm
 #Special Fields
 from wtforms import StringField, PasswordField, SubmitField
+#Flask validation package
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label = 'User Name')
-    email_address = StringField(label = 'Email Address:')
-    password1 = PasswordField(label = 'Password')
-    password2 = PasswordField(label = 'Confirm Password')
+    username = StringField(label = 'User Name', validators = [Length(min=3,max=24), DataRequired()])
+    email_address = StringField(label = 'Email Address:',validators = [Email(), DataRequired()])
+    password1 = PasswordField(label = 'Password', validators = [Length(min=6), DataRequired()])
+    password2 = PasswordField(label = 'Confirm Password',validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label = 'Create Account')
